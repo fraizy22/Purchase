2f667be4f74251c33e3066534022c6d8eb805f7e
Your Name <you@example.com>
2020-02-27 11:58:56 +0100

    Format prices added Readme.md about the program added

---
+++ Readme.md
@@ -0,0 +1,6 @@
+Purchase
+
+An example program with a goods basket, goods and purchasing.
+
+It gives the sales check at execution, when enough money in wallet or prints the missing amount of money.
+
--- src/FormatPrice.java
+++ src/FormatPrice.java
@@ -1,7 +1,15 @@
 /**
+ * Format prices to fixed two digits after comma
+ *
  * @author fs - Frederick Stallmeyer
  * @version 1.0
  */
 
 public class FormatPrice {
+
+    static String formatPrice(double price) {
+        double round = Math.round(price * 100.0) / 100.0;
+        return String.format("%.2f", round);
+    }
+
 }
